a617676dc3be9b8c7be48b6d9e2e3198bec2ac03
damartripamungkas <82322639+damartripamungkas@users.noreply.github.com>
2021-12-17 00:07:08 +0700

    UPDATE V8.1.5

--- config/config.js
+++ config/config.js
@@ -1,50 +1,63 @@
 /**
  * edit the configuration below using the help of syntax highlighting to make it easier
  * contact the author to ask questions regarding the configuration below
+ * 
+ * ================================== TIPS =======================================
+ * for network cronos use gwei minimum 5000
  */
+
 module.exports = {
     allUrl: {
-        urlExplorer: "https://bscscan.com", // url explorer blockchain, example bscscan, etherscan, etc, format INPUT is TEXT.
-        urlMainNode: "wss://bsc-ws-node.nariox.org:443", // custom node url support ipc/http/wss for swap transaction, format INPUT is TEXT.
-        urlSecondNode: "https://bsc-dataseed1.ninicoin.io/", // node url support http/wss for checker, format INPUT is TEXT.
-        urlSignInNode: "https://bsc-dataseed2.ninicoin.io/", // node url binanceSmartChain(BSC) support ipc/http/wss for signin in the bot, format INPUT is TEXT.
+        urlMainNode: "wss://bsc-ws-node.nariox.org:443", // node blockchain rpc support ipc/http/wss for swap transaction, format INPUT is TEXT.
+        urlSecondNode: "https://bsc-dataseed1.ninicoin.io/", // node blockchain rpc support ipc/http/wss for checker, wait transaction status, format INPUT is TEXT.
     },
     myWallet: {
-        privateKeyOrMnemonic: "pharse or private key 0x1234567890", // your wallet mnemonic pharse or privateKey, format INPUT is TEXT.
+        privateKeyOrMnemonic: "WALLET ENCRYPTION KEY AES" // your wallet mnemonic pharse or privateKey, format INPUT is TEXT.
     },
     allAddress: {
         router: "0x10ED43C718714eb63d5aA57B78B54704E256024E", // address router with structur code UniswapV2, format INPUT is address.
-        honeypotIs: "0x2bf75fd2fab5fc635a4c6073864c708dfc8396fc", // function for "antiScamTokenCantSell", format INPUT is address.
-        chiGasToken: "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c", // chi gas token for reduce transaction fees, for botexploit, format INPUT is address.
-        customRouter: "0x0309bc0d9ce3501d55f768f307cdbdbd6391eaaa", // custom router for swap tx, format INPUT is address.
-        addressSignIn: "0x0309bc0d9ce3501d55f768f307cdbdbd6391eaaa", // address to signin for account in the bot, format INPUT is address.
         addressTokenUsedToBuy: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c", // [TOKEN-0] address that will be used to buy token, format INPUT is address.
-        addressTokenOrPresale: "0x9fdc3ae5c814b79dca2556564047c5e7e5449c19", // [TOKEN-1] address token or presale to buy, format INPUT is address.
-        storeAddressWrappedCoin: [
-            { address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", chainid: "1" }, // WETH
-            { address: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c", chainid: "56" }, // WBNB
-            { address: "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23", chainid: "25" }, // WCRO
-        ],
+        addressTokenOrPresale: "0xe9e7cea3dedca5984780bafc599bd69add087d56", // [TOKEN-1] address token or presale to buy, format INPUT is address.
+        storingAddressAboutBot: [
+            {
+                chainid: "1", // network ETH
+                honeypotIs: "0x5bf62ec82af715ca7aa365634fab0e8fd7bf92c7",
+                wrappedCoin: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+                chiGasToken: "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c",
+                customRouter: "none"
+            },
+            {
+                chainid: "25", // network CRO
+                honeypotIs: "none",
+                wrappedCoin: "0x5c7f8a570d578ed84e63fdfa7b1ee72deae1ae23",
+                chiGasToken: "none",
+                customRouter: "none"
+            },
+            {
+                chainid: "56", // network BNB
+                honeypotIs: "0x2bf75fd2fab5fc635a4c6073864c708dfc8396fc",
+                wrappedCoin: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c",
+                chiGasToken: "0x0000000000004946c0e9F43F4Dee607b0eF1fA1c",
+                customRouter: "0x00c3bD2BaA40096E79617135A8ee0E18a8BDCEF7"
+            },
+        ]
     },
     transaction: {
         gwei: "auto", // gwei transaction, format INPUT is NUMBER, TEXT(auto).
-        amount: "0.05", // amount token to buy token. format INPUT is NUMBER.
-        gasLimit: "300000", // gas limit transaction. format INPUT is NUMBER.
+        amount: "0.00001", // amount token to buy token. format INPUT is NUMBER.
+        gasLimit: "1000000", // gas limit transaction. format INPUT is NUMBER.
         waitTxStatus: "disable", // wait transaction status success or failed, format INPUT is TEXT(enable, disable).
         botsniper: {
             intervalCheck: "500", // input time mileseconds for interval check, format INPUT is NUMBER.
             optionSwap: {
                 buy: {
                     antiscamtokencantsell: "disable", // only support pair WBNB or BNB, example TOTEN/WBNB, TOTEN/BNB, if liquidity not WBNB or BNB set this to disable. format INPUT is TEXT(enable, disable)
-                    waitingForLiquidity: "disable", // if this is set to disable, bot wil buy token without liquidity, format INPUT is TEXT(enable, disable).
+                    waitingForLiquidity: "enable", // if this is set to disable, bot wil buy token without liquidity, format INPUT is TEXT(enable, disable).
                     targetBulk: "1", // target bulk for swap, this method using SCRIPT to send bulk transaction with different transaction, format INPUT is NUMBER.
                     timeout: "disable", // delay seconds before buy token for bypass antibot on token, format INPUT is NUMBER, TEXT(disable).
                 },
                 sell: {
-                    /**
-                     * autoTradeSellToken
-                     * if you use "takeprofit" or "cutlose" you must be set "useSellToken" to enable
-                     */
+                    // [autoTradeSellToken]. if you use "takeprofit" or "cutlose" you must be set "useSellToken" to enable
                     useSellToken: "enable", // set enable and bot will sell token after buy token, format INPUT is TEXT(enable, disable).
                     takeprofit: "1", // percentage increase in token price, format INPUT is NUMBER, TEXT(disable).
                     cutlose: "99", // percentage decrease in token price, max 100, format INPUT is NUMBER, TEXT(disable).
@@ -52,7 +65,7 @@ module.exports = {
             },
             optionPresale: {
                 // if you set targetBulk upper 1 you must be set "waitingForUnlock" to disable
-                waitingForUnlock: "enable", // if this is set to disable, bot wil send bnb without status unlock, format INPUT is TEXT(enable, disable)
+                waitingForUnlock: "enable", // if this is set to disable, bot will send coin without status unlock, format INPUT is TEXT(enable, disable)
                 targetBulk: "1", // target bulk for send tx, this method using SCRIPT to send bulk transaction with different transaction, format INPUT is NUMBER
             }
         },
@@ -60,10 +73,10 @@ module.exports = {
             /**
              * I explain the features "use" below
              * default = meaning the bot will filter transactions using the PREMIUM mempool of this bot without disturbing "urlMainNode", and after finding a matching transaction the bot will send transactions using "urlMainNode". only support network BSC, ETH, MATIC, POLYGON
-             * custom = means the bot will filter transactions using "urlMainNode", and after finding a matching transaction the bot will send transactions using "urlMainNode"
+             * custom = means the bot will filter transactions using "urlMainNode", and after finding a matching transaction the bot will send transactions using "urlMainNode". support all network
              */
             use: "default", // if your have private node GETH, set this is to custom and set if no have set this is to default, format INPUT is TEXT(default, custom).
-            key: "asdhaasd-adasdwad-as3434343", // function for default, contact author to get key, format INPUT is TEXT.
+            key: "key from author", // function for default, contact author to get key, format INPUT is TEXT.
             customRouter: {
                 /**
                  * about customRouter:
@@ -109,10 +122,7 @@ module.exports = {
                         usefilterMethodTokenFromAddress: "disable", // if you want to use "filterMethodTokenFromAddress" set this to enable, format INPUT is TEXT(enable, disable)
                     },
                     sell: {
-                        /**
-                         * autoTradeSellToken
-                         * if you use "takeprofit" or "cutlose" you must be set "useSellToken" to enable
-                         */
+                        // [autoTradeSellToken]. if you use "takeprofit" or "cutlose" you must be set "useSellToken" to enable
                         useSellToken: "disable", // set enable and bot will sell token after buy token, format INPUT is TEXT(enable, disable).
                         takeprofit: "200", // percentage increase in token price, format INPUT is NUMBER, TEXT(disable).
                         cutlose: "50", // percentage decrease in token price, format INPUT is NUMBER, TEXT(disable).
@@ -139,11 +149,12 @@ module.exports = {
                      * - botexploit/customRouter
                      * - botexploit/sandwich
                      */
-                    delayAfterTx: "5", // delay seconds after your tx sandwich, format INPUT is NUMBER, TEXT(disable).
-                    multipleGweiTxBeforeTargetTx: "1.3", // the value must be more than 1. example of how it works, the target of buying BUSD tokens with 5 gwei bots will do multiple gwei targets with this set and the results of the gwei calculations will be used for the first BUY, format INPUT is NUMBER.
+                    slipPage: "20", // slippage transaction to prevent being sandwiched by other bots, input 0 for unlimited slippage, format INPUT is NUMBER.
+                    delayAfterTx: "200", // delay seconds after your tx sandwich, format INPUT is NUMBER, TEXT(disable).
+                    multipleGweiTxBeforeTargetTx: "1.1", // the value must be more than 1. example of how it works, the target of buying BUSD tokens with 5 gwei bots will do multiple gwei targets with this set and the results of the gwei calculations will be used for the first BUY, format INPUT is NUMBER.
                     filterMinimumAmountTarget: {
-                        coin: "0.01", // minimum amount swap target in the number of coin target example BNB, ETH, CRO, format INPUT is NUMBER.
-                        token: "0.01" // minimum amount swap target in the number of token target [TOKEN-1], format INPUT is NUMBER.
+                        coin: "1", // minimum amount swap target in the number of coin target example BNB, ETH, CRO, format INPUT is NUMBER.
+                        token: "5", // minimum amount swap target in the number of token target [TOKEN-1], format INPUT is NUMBER.
                     },
                 },
                 buyBulk: {
